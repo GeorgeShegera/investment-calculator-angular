163e6dcd0717977ec7d02b48386edb2107cdbfb7
George Shegera <shegerageorgy@gmail.com>
2025-07-01 18:36:22 +0300

    currency-pipes

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
 
 import { InvestmentInput } from '../investment-input.model';
 import { InvestmentResult } from '../investment-result.model';
 
 @Component({
   selector: 'app-investment-results',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.scss',
 })
