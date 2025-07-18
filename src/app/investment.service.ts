5d9cb66bd86a598e3606982b38443b1d34c9bc5c
George Shegera <shegerageorgy@gmail.com>
2025-07-18 18:28:08 +0300

    signals in service

--- src/app/investment.service.ts
+++ src/app/investment.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { InvestmentInput } from './investment-input.model';
 import { InvestmentResult } from './investment-result.model';
 
@@ -6,7 +6,7 @@ import { InvestmentResult } from './investment-result.model';
   providedIn: 'root',
 })
 export class InvestmentService {
-  resultsData?: InvestmentResult[];
+  resultsData = signal<InvestmentResult[] | undefined>(undefined);
 
   calculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment, duration, expectedReturn, annualInvestment } =
@@ -31,6 +31,6 @@ export class InvestmentService {
       });
     }
 
-    this.resultsData = annualData;
+    this.resultsData.set(annualData);
   }
 }
