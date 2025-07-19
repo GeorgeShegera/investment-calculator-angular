cff629226df396dca1a7345131c0821535f14def
George Shegera <shegerageorgy@gmail.com>
2025-07-19 16:25:10 +0300

    Angular Update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
+import { AppModule } from './app/app.module';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { importProvidersFrom } from '@angular/core';
 
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+bootstrapApplication(App, { providers: [importProvidersFrom(AppModule)] });
